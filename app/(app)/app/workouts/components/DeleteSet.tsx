1eaacf0750c0c71127fbf630c39bf13f4e51b7b9
Thomas <4428811+ThomasRoest@users.noreply.github.com>
2025-02-12 15:28:00 +0100

    add action menu

--- app/(app)/app/workouts/components/DeleteSet.tsx
+++ app/(app)/app/workouts/components/DeleteSet.tsx
@@ -1,23 +1,52 @@
-"use client"
+"use client";
 
 import { deleteSet } from "@/actions/sets/delete";
-import { DeleteButton } from "@/components/DeleteButton";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { toastOptions } from "@/lib/utils";
 import { Set } from "@prisma/client";
-import { useCallback, useTransition } from "react";
+import { Loader2, MoreHorizontal, Trash } from "lucide-react";
+import { useCallback, useState, useTransition } from "react";
 import toast from "react-hot-toast";
 
 export const DeleteSet = ({ set }: { set: Set }) => {
+  const [open, setIsOpen] = useState(false);
   const [isPending, startTransition] = useTransition();
 
-  const handleClick = useCallback(async () => {
+  const onDelete = useCallback(() => {
     startTransition(async () => {
       const result = await deleteSet(set);
       if (result.success) {
         toast.success("Deleted", toastOptions);
+        setIsOpen(false);
       }
     });
-  }, [set]);
+  }, []);
 
-  return <DeleteButton onClick={handleClick} isPending={isPending} />;
+  return (
+    <DropdownMenu onOpenChange={setIsOpen} open={open}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={onDelete}>
+          <Trash className="mr-2 h-4 w-4" />
+          Delete
+          {isPending && <Loader2 className="animate-spin" />}
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
