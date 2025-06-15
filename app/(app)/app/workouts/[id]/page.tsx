b6c091df433d38fd895c1f61c4ab7979ef185184
Thomas <4428811+ThomasRoest@users.noreply.github.com>
2025-06-15 12:23:46 +0200

    darkmode fix

--- app/(app)/app/workouts/[id]/page.tsx
+++ app/(app)/app/workouts/[id]/page.tsx
@@ -39,10 +39,10 @@ const WorkoutPage = async (props: { params: Promise<{ id: string }> }) => {
       </PageHeader>
       {workout.note && (
         <>
-          <div className="p-3 mb-4 rounded-lg bg-gray-50 flex  items-center justify-between">
+          <div className="p-3 mb-4 rounded-lg bg-gray-50 flex  items-center justify-between dark:bg-gray-800 dark:text-gray-400">
             <div className="flex items-center gap-x-3">
               <MessageSquareText className="text-slate-500 w-5 h-5" />
-              <p className="text-sm text-gray-700">{workout.note}</p>
+              <p className="text-sm text-gray-700 dark:text-gray-400">{workout.note}</p>
             </div>
             <DeleteWorkoutNote workout={workout} />
           </div>
--- app/(app)/app/workouts/components/DeleteButton.tsx
+++ app/(app)/app/workouts/components/DeleteButton.tsx
@@ -9,7 +9,7 @@ export const DeleteButton = () => {
     return <Loader2 className="animate-spin h-5 w-5 text-slate-300" />;
   }
   return (
-    <button className="text-xs bg-slate-300 text-white h-5 w-5 rounded-md">
+    <button className="text-xs bg-slate-300 text-white h-5 w-5 rounded-md dark:bg-gray-800 dark:hover:bg-gray-600">
       x
     </button>
   );
