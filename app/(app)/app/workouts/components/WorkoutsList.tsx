7fbe289129e8ef984d89f4e8d6c12c10743bd52e
Thomas <4428811+ThomasRoest@users.noreply.github.com>
2025-12-07 21:27:33 +0100

    remove unused

--- app/(app)/app/workouts/components/WorkoutsList.tsx
+++ app/(app)/app/workouts/components/WorkoutsList.tsx
@@ -1,6 +1,5 @@
 import { getUserWorkouts } from "@/data/workouts";
 import { WorkoutItem } from "./WorkoutItem";
-import { useToast } from "@/lib/useToast";
 
 export const WorkoutsList = async ({ year }: { year: string }) => {
   const workouts = await getUserWorkouts({ year: parseInt(year) });
@@ -11,7 +10,6 @@ export const WorkoutsList = async ({ year }: { year: string }) => {
 
   return (
     <ul className="space-y-2">
-      <button>click</button>
       {workouts.map((workout) => {
         return <WorkoutItem key={workout.id} workout={workout} />;
       })}
